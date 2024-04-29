3a4823aa0307d9a4cbaeb419fbada20270fe2ea5
Jaimie <79995651+jaimiem17@users.noreply.github.com>
2024-04-29 02:29:47 +0000

    complete

--- answers.md
+++ answers.md
@@ -1,8 +1,35 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:** Jaimie Morris
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
+
+1a) To produce as few coins as possible that sum to $N$:
+1. Initialize an empty list `coins` to store the coins used.
+2. Iterate through coin denominations in decreasing order (starting from the highest denomination).
+3. While the remaining amount $N$ is greater than 0:
+   a. If the current denomination is less than or equal to $N$, subtract it from $N$ and add it to `coins`.
+   b. Otherwise, move to the next smaller denomination.
+4. Return the list of coins.
+
+1b) At each step, selecting the largest possible coin, $2^k$, denomination ensures the minimum total coins used. Optimal solutions to subproblems (smaller remaining amounts) contribute to the optimal solution for the overall problem.
+
+1c) work and span are both O(logn)
+
+2a) a counterexample would be to let the denominations be [4, 3, 1] and the amount be $6.
+The greedy algorithm would choose [4, 1, 1] (3 coins),
+but the optimal solution is [3, 3] (2 coins)
+
+2b) The optimal solution would be to find the minimum number of coins to get $N$ dollars. For any amount $N$ and denomination list [D0, D1, ..., Dk], the optimal solution can be recursively determined by considering all choices of using or not using each denomination.
+
+2c)  By using a bottom-up approach, it would start by initializing a table to store minimum coins for subproblems, ensuring optimal solutions to smaller subproblems are calculated first. Then, we iteratively fill in the table by considering all possible choices of using or not using each denomination for each amount, leading to the calculation of the minimum coins needed for the target amount N in DP[N][k]
+Work = O(N*k), where N is the amount and k is the number of denominations and Span = O(k), as each subproblem computation depends on previous subproblems within the denomination loop.
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -3,27 +3,84 @@ from collections import Counter
 
 ####### Problem 3 #######
 
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
+test_cases = [('book', 'back'), ('kookaburra', 'kookybird'),
+              ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
+alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'),
+              ('relev--ant', '-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
+
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+  # TO DO - modify to account for insertions, deletions and substitutions
+  if (S == ""):
+    return (len(T))
+  elif (T == ""):
+    return (len(S))
+  else:
+    if (S[0] == T[0]):
+      return (MED(S[1:], T[1:]))
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+      return (1 + min(MED(S, T[1:]), MED(S[1:], T)))
+
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+  # TODO -  implement top-down memoization
+  def fast_MED(S, T, MED={}):
+    if (S, T) in MED:
+      return MED[(S, T)]
+
+    if S == "":
+      result = len(T)
+    elif T == "":
+      result = len(S)
+    else:
+      if S[0] == T[0]:
+        result = fast_MED(S[1:], T[1:], MED)
+      else:
+        result = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED),
+                         fast_MED(S[1:], T[1:], MED))
+
+    MED[(S, T)] = result
+    return result
+
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+  # TODO - keep track of alignment
+  if (S, T) in MED:
+    return MED[(S, T)]
 
+  if S == "":
+    alignment_S = "-" * len(T)
+    alignment_T = T
+  elif T == "":
+    alignment_S = S
+    alignment_T = "-" * len(S)
+  elif S[0] == T[0]:
+    A_S, A_T = fast_align_MED(S[1:], T[1:], MED)
+    alignment_S = S[0] + A_S
+    alignment_T = T[0] + A_T
+  else:
+    I_S, I_T = fast_align_MED(S, T[1:], MED)
+    D_S, D_T = fast_align_MED(S[1:], T, MED)
+    I_C = 1 + len(I_S)
+    D_C = 1 + len(D_S)
+    if I_C <= D_C:
+      alignment_S = "-" + I_S
+      alignment_T = T[0] + I_T
+    else:
+      alignment_S = S[0] + D_S
+      alignment_T = "-" + D_T
+
+  alignment_result = (alignment_S, alignment_T)
+  MED[(S, T)] = alignment_result
+  return alignment_result
+
+
+
+for S, T in test_cases:
+  assert fast_MED(S, T) == MED(S, T)
+
+for i in range(len(test_cases)):
+  S, T = test_cases[i]
+  align_S, align_T = fast_align_MED(S, T)
+  assert (align_S == alignments[i][0] and align_T == alignments[i][1])
